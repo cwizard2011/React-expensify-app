2c060fc3975778e7d431ccbd1e7824d45bb72208
cwizard2011 <cwizard2011@gmail.com>
2018-01-18 09:43:43 +0100

    PublicRoute

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboard';
 import AddExpensePage from '../components/AddExpense';
 import EditExpensePage from '../components/EditExpense';
-import HelpPage from '../components/Help';
 import NotFoundPage from '../components/NotFound';
 import LoginPage from '../components/LoginPage';
-import PrivateRoute from './PrivateRoute'
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
       <div>
         <Switch>
-        <Route path= "/" component ={LoginPage} exact={true}/>
+        <PublicRoute path= "/" component ={LoginPage} exact={true}/>
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
        </Switch>
       </div>
