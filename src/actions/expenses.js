0178b87e840341247c11db365083319491d7cfd1
cwizard2011 <cwizard2011@gmail.com>
2018-01-21 16:12:54 +0100

    codacy fix

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -29,55 +29,55 @@ export const startAddExpense = (expenseData = {}) => {
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
+  type: 'REMOVE_EXPENSE',
+  id
 })
 
 export const startRemoveExpense = ({ id } = {}) => {
-    return (dispatch, getState) => {
-        const uid = getState().auth.uid;
-       return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
-           dispatch(removeExpense({ id }));
+  return (dispatch, getState) => {
+      const uid = getState().auth.uid;
+      return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
+        dispatch(removeExpense({ id }));
        });
-    };
+  };
 };
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
-    id,
-    updates
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
 
 export const startEditExpense = (id, updates) => {
-    return (dispatch, getState) => {
-        const uid = getState().auth.uid;
-        return database.ref(`users/${uid}expenses/${id}`).update(updates).then(() => {
-            dispatch(editExpense(id, updates));
-        });
-    };
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+      return database.ref(`users/${uid}expenses/${id}`).update(updates).then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
 };
 
 //SET_EXPENSES
 export const setExpenses = (expenses) => ({
-    type: 'SET_EXPENSES',
-    expenses
+  type: 'SET_EXPENSES',
+  expenses
 });
 
 export const startSetExpenses = () => {
-    return (dispatch, getState) => {
-        const uid = getState().auth.uid;
-       return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
-            const expenses = [];
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
+      const expenses = [];
 
-            snapshot.forEach((childSnapshot) => {
-                expenses.push({
-                    id: childSnapshot.key,
-                    ...childSnapshot.val()
-                });
-            });
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
 
-            dispatch(setExpenses(expenses));
-        })
-    }
+      dispatch(setExpenses(expenses));
+    })
+  }
 }
